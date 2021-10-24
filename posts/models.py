4c04e9027729e4a82cce75aa43aff0b687a972d2
Young Soo Koh <kohys92@gmail.com>
2021-10-25 00:06:18 +0900

    [NOTICEBOARD database modelling] - Add: posts and users models 생성 - Modify: CASCADE error

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Posts(models.Model):
+    title      = models.CharField(max_length=200)
+    author     = models.CharField(max_length=100)
+    content    = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    user       = models.ForeignKey('users.User', on_delete = models.CASCADE)
+
+    class Meta:
+        db_table = 'posts'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username   = models.CharField(max_length=50)
+    email      = models.EmailField(unique=True)
+    password   = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'users'
